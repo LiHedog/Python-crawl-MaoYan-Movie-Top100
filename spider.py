7f093ddaeb28997a0a0af165a61fd94812431f16
kende <kendedonglan@gmail.com>
2019-03-03 21:37:40 +0800

    Python-crawl-MaoYan-Movie-Top100

---
+++ spider.py
@@ -0,0 +1,51 @@
+import requests, re, json
+from requests.exceptions import RequestException
+
+#from multiprocessing import Pool
+#pool = Pool()
+#pool.map(main, [i * 10 for i in range(10)])
+
+#获取链接
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+#解析源码
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         +'.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         +'.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield{
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score':item[5]+item[6]
+        }
+
+#写到文件中去
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+        f.close()
+
+#主函数
+def main(offset):
+    url = 'https://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+#执行主函数
+if __name__ == '__main__':
+    for i in range(10):
+        main(i*10)
